05601966aa01cd6d519b40d6e61f1b405fdb36ee
heejy <heejy@pm.me>
2022-10-25 10:58:36 +0200

    create ui for the game

--- script.js
+++ script.js
@@ -27,29 +27,49 @@ function playRound (playerSelection, computerSelection){
         }
     }
 }
+
+
+const buttons = document.querySelectorAll('.selection');
+const resultsDiv = document.querySelector('.results')
+let h1 = document.getElementById("result")
+
 const NUM_OF_POINTS = 3;
 let playerPoints = 0;
 let computerPoints = 0;
-while (playerPoints < NUM_OF_POINTS && computerPoints < NUM_OF_POINTS) {
-    let playerSelection = prompt("R? P? S?").toLowerCase();
-    let computerSelection = getComputerChoice();
-    let winner = playRound(playerSelection, computerSelection);
-    if (winner === "player") {
-        playerPoints++;
-        console.log(`${playerSelection} vs ${computerSelection}: you won! (${playerPoints}:${computerPoints})`)
-    } else if (winner ===  "computer") {
-        computerPoints++;
-        console.log(`${playerSelection} vs ${computerSelection}: you lost! (${playerPoints}:${computerPoints})`)
-    }
-    else {
-        console.log(`${playerSelection} vs ${computerSelection}: draw! (${playerPoints}:${computerPoints})`)
-    }
- }
- let h1 = document.getElementById("result")
- if(computerPoints === NUM_OF_POINTS) {
-    h1.innerHTML = "You lost";
- }
- else if(playerPoints === NUM_OF_POINTS) {
-    h1.innerHTML = "You won!";
- }
+
+
+
+
+buttons.forEach((button) => {
+    console.log(button)
+    button.addEventListener('click', (e) => {
+        //console.log( playRound(selection, getComputerChoice()) )
+
+        let playerSelection = e.target.id;
+        let computerSelection = getComputerChoice();
+        const paragraph = document.createElement('p');
+        let winner = playRound(playerSelection, computerSelection);
+        if (winner === "player") {
+            playerPoints++;
+            paragraph.textContent = `${playerSelection} vs ${computerSelection}: you won! (${playerPoints}:${computerPoints})`
+        } else if (winner ===  "computer") {
+            computerPoints++; 
+            paragraph.textContent = `${playerSelection} vs ${computerSelection}: you lost! (${playerPoints}:${computerPoints})`
+        }
+        else {
+            paragraph.textContent = `${playerSelection} vs ${computerSelection}: draw! (${playerPoints}:${computerPoints})`
+        }
+        resultsDiv.appendChild(paragraph)
+
+        if(computerPoints === NUM_OF_POINTS) {
+            h1.innerHTML = "You lost";
+         }
+         else if(playerPoints === NUM_OF_POINTS) {
+            h1.innerHTML = "You won!";
+         }
+
+    })
+} )
+
+
 
